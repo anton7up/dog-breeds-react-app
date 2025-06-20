95d21170718aaa90707e6e82bda450d39326113a
anton7up <99725170+anton7up@users.noreply.github.com>
2025-06-20 07:56:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';  // <-- добавь этот импорт
 
 export default defineConfig({
   base: '/dog-breeds-react-app/',  // Имя твоего репозитория
